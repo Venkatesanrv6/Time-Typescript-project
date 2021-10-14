5f78c203b51431673ff4ac85afab57e41bb4bf26
Venkatesanrv6 <venkatesanrv6@gmail.com>
2021-10-14 19:24:42 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# typescript-dkk49d
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/typescript-dkk49d)
---
+++ index.ts
@@ -0,0 +1,3 @@
+
+let today: Date = new Date(); 
+console.log('your data:', today.getDate(),today.getMonth(),today.getFullYear(),today.getHours(), today.getMinutes(),today.getSeconds(),today.getMilliseconds() ); 
